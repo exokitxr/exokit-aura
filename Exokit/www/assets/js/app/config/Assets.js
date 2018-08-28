108b6a5a3fabc9cf7f56d445c014121694552274
Michael Anthony <michael@activetheory.net>
2018-08-27 19:18:49 -0700

    no message

--- Exokit/www/assets/js/app/config/Assets.js
+++
@@ -1,2 +0,0 @@
-window.ASSETS = ["assets/data/uil.json","assets/shaders/compiled.vs"];
-ASSETS.SW = ["assets/css/style.css","assets/js/app.js"];
--- Exokit/www/assets/js/app/util/tests/Tests.js
+++
@@ -1,8 +0,0 @@
-Class(function Tests() {
-    const _this = this;
-
-    this.getDPR = function() {
-        return Device.pixelRatio;
-    }
-
-}, 'static');
