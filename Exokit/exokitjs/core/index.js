e89c5a32d4a1ad7302f8bc06c6f43ed1872636f7
hyperandroid <hyperandroid@gmail.com>
2018-09-15 21:11:54 +0200

    remove example code.

--- Exokit/exokitjs/core/index.js
+++ Exokit/exokitjs/core/index.js
@@ -26,37 +26,3 @@ EXOKIT.onload = function() {
 window.requestAnimationFrame = function(callback) {
     EXOKIT.animationFrame = callback;
 };
-
-//var f1 = File.create("www/index.html", 0);   // bundle
-//console.log(f1.loadAsText());
-//var f2 = File.create("https://mozilla.com", 2);   // remote
-//console.log(f2.loadAsText());
-
-
-var et = EventTarget.create();
-et.addEventListener("abcd", function(e) { console.log("recevied type: "+e.type) } );
-function fn(e) { console.log("recevied type: "+e.type) }
-et.addEventListener("abcd", fn );
-et.addEventListener("xyz", function(e) { console.log("recevied type: "+e.type) } );
-
-et.dispatchEvent(Event.create('abcd'));
-et.dispatchEvent({type:"xyz"});
-et.dispatchEvent({type:"none"});
-console.log('remove');
-et.removeEventListener( "abcd", fn );
-et.dispatchEvent({type:"abcd"});
-et.dispatchEvent({type:"xyz"});
-et.dispazztchEvent({type:"none"});
-
-et= null;
-
-console.log("XHR insrtance of EventTarget= "+(XMLHttpRequest instanceof EventTarget));
-var xhr = XMLHttpRequest.create();
-for(var i in xhr) {
-    console.log(i);
-}
-xhr.addEventListener("abcd", function(e) { console.log("recevied type: "+e.type) } );
-xhr.emit("abcd",{type:"none"});
-
-
-garbageCollect()
